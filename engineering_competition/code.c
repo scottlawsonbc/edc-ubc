1e4aba48124c1a2cee83db42a3a5b3f62f4b823c
Scott Lawson <scottlawsonbc@gmail.com>
2015-10-17 12:30:56 -0700

    API

--- engineering_competition/code.c
+++ engineering_competition/code.c
@@ -2,26 +2,82 @@
 #include "API.h"
 #include <stdint.h>
 
-void setup(void) {
-    
-    // define pins
-    uint_32 left_tape_pin = 0;
-    uint_32 right_tape_pin = 1;
-    uint_32 block_pin = 1;
+/* Sensor pin definitions */
+#define LEFT_SENSOR_PIN  1
+#define RIGHT_SENSOR_PIN 2
+#define BLOCK_DETECT_PIN 3
+#define GRAB_DETECT_PIN  4
+
+/* Motor pin definitions */
+#define LEFT_MOTOR_PIN  1
+#define RIGHT_MOTOR_PIN 2
+#define LIFT_MOTOR_PIN  3
+#define GRAB_MOTOR_PIN  4
+
+/* If sensor > DarkThreshold, then sensor is on tape */
+int32_t LeftDarkThreshold  = 0;
+int32_t RightDarkThreshold = 0;
+
+/* Motor speed parameters */
+/* Motor functions will limit speeds to these values */
+int32_t MaxMoveSpeed = 0;
+int32_t MaxLiftSpeed = 0;
+int32_t MaxGrabSpeed = 0;
+int32_t MinMoveSpeed = 0;
+int32_t MinLiftSpeed = 0;
+int32_t MinGrabSpeed = 0;
+
+/* PID gain values */
+float GainP = 0;
+float GainI = 0;
+float GainD = 0;
+float IntegralErrorMax = 0;
+
+/* PID error values */
+float PIDCurrentError = 0;
+float PIDPreviousError = 0;
+
+/* Raw analog sensor values */
+volatile int32_t LeftAnalogRaw;
+volatile int32_t RightAnalogRaw;
+
+/* Sensor states */
+volatile bool LeftDetectsTape;
+volatile bool RightDetectsTape;
+volatile bool BlockDetected;    /* HIGH = microswitch pressed */
+volatile bool BlockGrabbed;     /* HIGH = microswitch pressed */
+
+/* Error > 0: Too left */
+/* Error < 0: Too right */
+volatile int32_t CurrentTapeError;
+volatile int32_t PreviousTapeError;
+
+/* Speed control of left and right motors) */
+void LeftSpeed(int32_t speed);
+void RightSpeed(int32_t speed);
+
+/* Raise or lower the grabbing arm */
+/* speed: How quickly to raise/lower arm. Positive speed = RAISE. Negative speed = LOWER */
+/* duration_ms: Duration to move the arm in milliseconds */
+void Raise(int32_t speed, int32_t duration_ms);
+
+/* Pull or unwind the string */
+/* speed: How quickly to raise/lower arm. Positive speed = WIND. Negative speed = UNWIND */
+/* duration_ms: Duration to move the arm in milliseconds */
+void Grab(int32_t speed, int32_t duration_ms);
+
+/* Single increment of tape following at the given speed */
+void TapeFollow(int32_t nominal_speed);
+
+void setup(void) 
+{    
 }
 
-void loop(void) {
-
-// Read from inputs
-uint_32 left_tape = readAnalogInput(left_tape_pin);
-uint_32 right_tape = readAnalogInput(right_tape_pin);
-boolean isAcquired = readDigitalInput(block_pin);
-
-// states
+void loop(void) 
+{
+}
 
 // start
-
-
 // ------ first block --------
 
 // RL corner --> move straight
@@ -62,7 +118,7 @@ boolean isAcquired = readDigitalInput(block_pin);
 // back up
 // left corner --> turn left
 // RL corner --> release block
-}
+
 
 // functions
 void move(uint_32 left, uint_32 right);
