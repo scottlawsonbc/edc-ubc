560f3b20fa6bea75c50288826825ae995b980ffd
Scott Lawson <scottlawsonbc@gmail.com>
2015-10-17 09:36:17 -0700

    initial commit

---
+++ engineering_competition/UserInclude.h
@@ -0,0 +1,9 @@
+// UserInclude.h : header file
+#ifndef USERINCLUDE_H_
+#define USERINCLUDE_H_
+
+void setup(void);
+void loop(void);
+
+#endif // USERINCLUDE_H_
+
---
+++ engineering_competition/code.c
@@ -0,0 +1,13 @@
+#include "Main.h"
+#include "API.h"
+#include <stdint.h>
+
+void setup(void)
+{
+}
+
+void loop(void)
+{
+}
+
+
